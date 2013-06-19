405c2afd3d00d50475527ee96a7e715fdd903b5b
Horacio G. de Oro <hgdeoro@gmail.com>
2013-06-19 13:42:06 -0300

    Quite ultimo path absoluto

--- alarma.sh
+++ alarma.sh
@@ -6,10 +6,13 @@
 #    nohup ./alarma.sh > /tmp/alarma.1.log 2> /tmp/alarma.2.log < /dev/null &
 #
 
+DIR=$(cd $(dirname $0); pwd)
+
 while /bin/true ; do
 	for dev in /dev/ttyACM* ; do
 		echo "Iniciando en dev $dev"
-		python /opt/PyArduinoProxy/bin/webserver_direct.py --info $dev
+		# python /opt/PyArduinoProxy/bin/webserver_direct.py --info $dev
+		python $DIR/bin/webserver_direct.py --info $dev
 	done
 	sleep 1
 done
