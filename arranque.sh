7cf118f17297d14aefacb1583d118bb839b48033
Horacio G. de Oro <hgdeoro@gmail.com>
2013-06-19 13:40:24 -0300

    Quite path absoluto

--- arranque.sh
+++ arranque.sh
@@ -3,6 +3,9 @@
 #
 # Para lanzar en background:
 #
-   nohup /opt/PyArduinoProxy/alarma.sh > /tmp/alarma.1.log 2> /tmp/alarma.2.log < /dev/null &
-#
 
+# nohup /opt/PyArduinoProxy/alarma.sh > /tmp/alarma.1.log 2> /tmp/alarma.2.log < /dev/null &
+
+DIR=$(cd $(dirname $0); pwd)
+
+nohup $DIR/alarma.sh > /tmp/alarma.1.log 2> /tmp/alarma.2.log < /dev/null &
